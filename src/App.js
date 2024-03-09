320a8b319981464a94386f63ade7d8b8a233a324
Diego Gaibor <diego.g@scopicsoftware.com>
2024-03-09 13:47:03 -0500

    added login form

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import {Slider} from "@mui/material";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
       <Routes>
-          <Route path={"/"} element={<Home/>}  />
+          <Route path={"/"} element={<Login/>}  />
+          <Route path={"/dashboard"} element={<Home/>}  />
       </Routes>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,55 @@
+import {Box, Button, TextField} from "@mui/material";
+import {useRef} from "react";
+import {db} from "../firebase";
+import { collection, query,where,getDocs } from 'firebase/firestore';
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const email = useRef("");
+    const password = useRef("");
+    const usersRef =  collection(db,'users');
+    const navigate = useNavigate();
+
+
+    const login = async (e) =>{
+        e.preventDefault();
+        
+        console.log(email.current.value, password.current.value)
+        const search = query(usersRef, where("email" , "==" , email.current.value))
+        
+        const result = await getDocs(search);
+        console.log(result.docs.length)
+        if (result.docs.length > 0 ){
+            const user =result.docs[0].data()
+            const user_id = result.docs[0].id
+            if (user.password === password.current.value) {
+                console.log("login success");
+                console.log("Redirect");
+                localStorage.setItem('user_logged', JSON.stringify(user_id));
+                navigate('/dashboard', { replace: true });
+                
+            }else{
+                console.log("Password incorrect")
+            }
+        }else{
+            console.log("User not found")
+        }
+        
+
+
+    }
+    return (
+        <>
+            <div>
+                <h1>Login</h1>
+            </div>
+            <Box component="form" onSubmit={login} sx={{p: 2, border: '1px dashed grey'}}>
+                <TextField label="Email" inputRef={email} className={"w-full my-4"} variant="outlined" type="email"/>
+                <TextField label="Password" inputRef={password} className={"w-full my-4"} variant="outlined" type="password"/>
+                <Button type="submit" className={"my-4"}  variant="contained">Login</Button>
+            </Box>
+        </>
+
+
+    );
+}
