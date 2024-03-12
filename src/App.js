ce635cb9e1c162f734ff77967521e0774b4e811f
Diego Gaibor <diego.g@scopicsoftware.com>
2024-03-11 21:51:24 -0500

    add new flat workflow

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import {Slider} from "@mui/material";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import AddFlat from "./pages/AddFlat";
+import Flats from "./pages/Flats";
 
 function App() {
   return (
       <Routes>
           <Route path={"/"} element={<Login/>}  />
           <Route path={"/dashboard"} element={<Home/>}  />
+          <Route path={"/flats"} element={<Flats/>}  />
+          <Route path={"/flats/new"} element={<AddFlat/>}  />
       </Routes>
   );
 }
---
+++ src/pages/AddFlat.js
@@ -0,0 +1,15 @@
+import FlatForm from "../components/FlatForm";
+
+export default function AddFlat() {
+
+    return (
+        <>
+            <div>
+                <h1>Add Flat</h1>
+            </div>
+            <div>
+                <FlatForm />
+            </div>
+        </>
+    );
+}
