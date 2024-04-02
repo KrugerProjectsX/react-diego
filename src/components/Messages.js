24822dde2b5fc9aee84ef2a1ed9b029fbfd3e234
Diego Gaibor <diego.g@scopicsoftware.com>
2024-04-01 19:43:22 -0500

    add messgaes

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,18 +1,25 @@
-import {useEffect, useState} from "react";
-import {collection, doc, getDoc, getDocs} from "firebase/firestore";
+import {useEffect, useRef, useState} from "react";
+import {addDoc, collection, doc, getDoc, getDocs, query, where} from "firebase/firestore";
 import {db} from "../firebase";
 import {Box, Button, TextField} from "@mui/material";
 
 export default function Messages({flatId}) {
     const ref = doc(db, "flats", flatId);
+    const refMessages = collection(db, "messages");
+
     const [flat, setFlat] = useState({});
     //type = view | type = create
     const [type, setType] = useState('create');
     const [messages, setMessages] = useState([]);
+    const messageInput= useRef('');
     
     const getMessages = async () => {
-      const refMessages = collection(db, "messages");
-      const dataMessages = await getDocs(refMessages);
+        const search = query(refMessages, where("flatId", "==", flatId));
+        const dataMessages = await getDocs(search);
+        const rows = dataMessages.docs.map((item) => {
+            return { ...item.data(), id: item.id }
+        });   
+        setMessages(rows);
     }
     
     const getFlat = async () => {
@@ -36,8 +43,18 @@ export default function Messages({flatId}) {
         }, []
     )
     
-    const handleSubmit = () => {
-        
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const message = messageInput.current.value;
+        const data = {
+            message: message,
+            flatId: flatId,
+            userId: JSON.parse(localStorage.getItem('user_logged'))
+            
+        }
+        await addDoc(refMessages, data);
+
     }
     return (
         
@@ -48,8 +65,7 @@ export default function Messages({flatId}) {
                 <Box
                     component={'form'}
                     onSubmit={handleSubmit}
-                    className="
-        flex
+                    className="flex
         flex-col
         items-center
         justify-center
@@ -58,10 +74,21 @@ export default function Messages({flatId}) {
         bg-gray-200
         shadow-md"
                 >
-                    <TextField type={'text'} label={'Message'}  multiline maxRows={10} minRows={10} className="mb-4" />
+                    <TextField type={'text'} label={'Message'} inputRef={messageInput}  multiline maxRows={10} minRows={10} className="mb-4" />
                     <Button type={'submit'} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Send</Button>
                 </Box>
             </>
+            }
+            { type === 'view' && 
+                <>
+                    {messages.map((item) => {
+                        return (
+                            <div key={item.id} className="border p-4 m-4 rounded-full bg-blue-100">
+                                <p className="text-center">{item.message}</p>
+                            </div>
+                        )
+                    })}
+                </>
             }
         </div>
     );
